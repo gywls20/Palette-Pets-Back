c84d2ad119cddd506fdb36878b5ed3382368a4e3
Kim-soung-won <rlatmddnjs0103@naver.com>
2024-05-24 14:28:46 +0900

    Object Storage 이미지 등록 기능 추가, 이메일 전송 기능 추가

---
+++ src/main/java/com/palette/palettepetsback/config/Mail/EmailResponseDTO.java
@@ -0,0 +1,10 @@
+package com.palette.palettepetsback.config.Mail;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+@Getter
+@AllArgsConstructor
+public class EmailResponseDTO{
+    private final String data;
+}
