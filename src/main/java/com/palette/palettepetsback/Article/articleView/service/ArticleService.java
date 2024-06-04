4649f9b17f741572c0dda46fe61f9504d2df6925
Kim-soung-won <rlatmddnjs0103@naver.com>
2024-06-04 12:42:16 +0900

    목록 길이 측정 추가

--- src/main/java/com/palette/palettepetsback/Article/articleView/service/ArticleService.java
+++ src/main/java/com/palette/palettepetsback/Article/articleView/service/ArticleService.java
@@ -51,11 +51,10 @@ public class ArticleService {
     @Transactional(readOnly = true)
     public List<ArticleResponseDTO> searchList(PageableDTO pd){
         int offset = (pd.getPage()-1) * PAGE_SIZE;
-        int limit = offset + PAGE_SIZE;
         QArticle qArticle = QArticle.article;
 
         PathBuilder<?> entityPath = new PathBuilder<>(Article.class, "article"); // 나는 Article Entity를 조회할거야
-        Order order = pd.getDir() ? Order.ASC : Order.DESC; // 오름차순, 내림차순 정하기
+        Order order = pd.getDir() ? Order.DESC : Order.ASC; // 오름차순, 내림차순 정하기
         List<OrderSpecifier> orderSpecifiers = new ArrayList<>(); // 정렬 조건 모음
         orderSpecifiers.add(new OrderSpecifier(order, entityPath.get(pd.getSort()))); // 정렬 조건 넣기
 
@@ -71,8 +70,10 @@ public class ArticleService {
                 .selectFrom(qArticle)
                 .where(where)
                 .orderBy(orderSpecifiers.toArray(new OrderSpecifier[orderSpecifiers.size()]))
-                .offset(offset).limit(limit)
+                .offset(offset).limit(PAGE_SIZE)
                 .fetch();
+        System.out.println("offset : "+offset);
+        System.out.println("Size : "+articles.size());
 
         List<ArticleResponseDTO> articleResponseDTOList = articles.stream()
                 .map(responseDTO -> new ArticleResponseDTO(responseDTO))
@@ -80,8 +81,7 @@ public class ArticleService {
 
         return articleResponseDTOList;
     }
-
-
+    @Transactional(readOnly = true)
     public List<ArticleResponseDTO> searchTest(String articleTags) {
         QArticle qArticle = QArticle.article;
 
@@ -94,18 +94,19 @@ public class ArticleService {
             where.or(qArticle.articleTags.like("%"+search+"%"));
             // where articleTage like concat("%"+"고양이"+"%") or articleTage like concat("%"+"강아지"+"%")
         }
-
         List<Article> articleList = jpaQueryFactory
                 .selectFrom(qArticle)
                 .where(where)
                 .fetch();
-
         List<ArticleResponseDTO> articleResponseDTOList = articleList.stream()
                 .map(responseDTO -> new ArticleResponseDTO(responseDTO))
                 .collect(Collectors.toList());
-
         return articleResponseDTOList;
-
+    }
+    @Transactional(readOnly = true)
+    public Integer count(String where) {
+        System.out.println(articleRepository.countByArticleTagsContaining(where).orElse(0));
+        return articleRepository.countByArticleTagsContaining(where).orElse(0);
     }
 }
 
