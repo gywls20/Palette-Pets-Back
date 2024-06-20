640c22c91c33a758a80808a65d2691e51584a7a1
DoDoBird <88835687+gyuchanlee@users.noreply.github.com>
2024-06-20 20:35:07 +0900

    feat: hotSpot List 캐싱 적용
    - hotSpot 리스트만 캐싱 적용
    - CRUD + 조회수 증가 시, 캐싱 초기화 및 쿼리 수행

--- src/main/java/com/palette/palettepetsback/hotSpot/service/HotSpotService.java
+++ src/main/java/com/palette/palettepetsback/hotSpot/service/HotSpotService.java
@@ -19,6 +19,9 @@ import com.palette.palettepetsback.member.entity.Member;
 import com.palette.palettepetsback.member.repository.MemberRepository;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.cache.annotation.CacheEvict;
+import org.springframework.cache.annotation.CachePut;
+import org.springframework.cache.annotation.Cacheable;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.multipart.MultipartFile;
@@ -39,6 +42,7 @@ public class HotSpotService {
     private final HotSpotStarPointRepository hotSpotStarPointRepository;
 
     //hotspot 저장 메서드
+    @CacheEvict(value = "hotSpotList", allEntries = true, cacheManager = "cacheManager")
     @Transactional
     public Long HotSpotInsert(HotSpotAddRequest dto) {
 
@@ -61,6 +65,7 @@ public class HotSpotService {
     }
 
     //hotspot 업데이트 메서드
+    @CacheEvict(value = "hotSpotList", allEntries = true, cacheManager = "cacheManager")
     @Transactional
     public void HotSpotUpdate(HotSpotUpdateRequest dto, MultipartFile[] files) {
 
@@ -90,6 +95,7 @@ public class HotSpotService {
     }
 
     //hotspot 삭제 메서드
+    @CacheEvict(value = "hotSpotList", allEntries = true, cacheManager = "cacheManager")
     @Transactional
     public void HotSpotDelete(Long id){
         HotSpot hotSpot = hotSpotRepository.findById(id)
@@ -99,6 +105,7 @@ public class HotSpotService {
     }
 
     //hotspot 리스트 쿼리 메서드
+    @Cacheable(value = "hotSpotList", cacheManager = "cacheManager")
     public List<HotSpotListResponse> getAllHotSpot(){
         List<HotSpot> hotSpotList = hotSpotRepository.findHotSpotList();
 
@@ -120,46 +127,8 @@ public class HotSpotService {
         return list;
     }
 
-    //hotspot 한 건 쿼리 메서드 -> imageHotSpot 값도 같이 필요
-    public HotSpotResponse getHotSpotDetail(Long hotSpotId){
-
-        HotSpot hotSpot = hotSpotRepository.findById(hotSpotId)
-                .orElseThrow(() -> new RuntimeException("명소 추천 게시글이 존재하지 않습니다."));
-
-        List<ImgHotSpot> imgHotSpots = hotSpot.getImgHotSpots();
-
-        List<ImgHotSpotResponse> imgHotSpotDtoList = new ArrayList<>();
-
-        for (ImgHotSpot imgHotSpot : imgHotSpots) {
-            ImgHotSpotResponse dto = ImgHotSpotResponse.builder()
-                    .imgHotSpotId(imgHotSpot.getId())
-                    .imgUrl(imgHotSpot.getImgUrl())
-                    .build();
-
-            imgHotSpotDtoList.add(dto);
-        }
-
-        HotSpotResponse result = HotSpotResponse.builder()
-                .hotSpotId(hotSpot.getId())
-                .memberNickname(hotSpot.getMember().getMemberNickname())
-                .createAt(hotSpot.getCreatedAt())
-                .modifiedAt(hotSpot.getModifiedAt())
-                .placeName(hotSpot.getPlaceName())
-                .simpleContent(hotSpot.getSimpleContent())
-                .content(hotSpot.getContent())
-                .address(hotSpot.getAddress())
-                .lat(hotSpot.getLat())
-                .lng(hotSpot.getLng())
-                .countViews(hotSpot.getCountViews())
-                .imgList(imgHotSpotDtoList)
-                .build();
-
-        return result;
-    }
-
     // 이미지 등록 메서드
     @Transactional
-//    public Long addHotSpotImage(HotSpot hotSpot, MultipartFile img) {
     public Long addHotSpotImage(Long hotSpotId, MultipartFile img) {
 
         HotSpot hotSpot = hotSpotRepository.findById(hotSpotId)
@@ -217,7 +186,7 @@ public class HotSpotService {
         return imgHotSpotDtoList;
     }
 
-    // 명소 글 + 이미지 쿼리 -> join
+    // 명소 글 + 이미지 쿼리 -> join ( 글 상세조회 )
     public HotSpotResponse getHotSpotWithImg(Long hotSpotId) {
         HotSpot hotSpot = hotSpotRepository.findById(hotSpotId)
                 .orElseThrow(() -> new RuntimeException("hotSpot not found"));
@@ -290,6 +259,7 @@ public class HotSpotService {
         return hotSpotStarPoint.getRating();
     }
 
+    @CacheEvict(value = "hotSpotList", allEntries = true, cacheManager = "cacheManager")
     @Transactional
     public void plusCountView(Long hotSpotId) {
         HotSpot hotSpot = hotSpotRepository.findById(hotSpotId)
