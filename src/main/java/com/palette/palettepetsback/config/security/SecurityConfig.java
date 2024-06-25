fdf78922a471fe75e8f166d40ba0d73092be219f
Kim-soung-won <rlatmddnjs0103@naver.com>
2024-06-25 20:20:22 +0900

    거래글 등록 권한 추가

--- src/main/java/com/palette/palettepetsback/config/security/SecurityConfig.java
+++ src/main/java/com/palette/palettepetsback/config/security/SecurityConfig.java
@@ -99,6 +99,7 @@ public class SecurityConfig {
 //                                .anyRequest().permitAll()
                                 // 댓글 관련
                                 .requestMatchers("/Get/comments/**").permitAll()
+                                .requestMatchers("/post/carrot").permitAll()
                         .anyRequest().authenticated()
                 );
         // jwt 관련 필터들 적용 - 로그인 / username&password 인증 / 로그아웃 필터
