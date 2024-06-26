8183999e63f377595e50d9c9db9f7d187b704fba
gywls20 <koj858@naver.com>
2024-06-26 17:09:22 +0900

    fix: 에러 수정

--- src/main/java/com/palette/palettepetsback/carrot/repository/CarrotLikeRepository.java
+++ src/main/java/com/palette/palettepetsback/carrot/repository/CarrotLikeRepository.java
@@ -5,8 +5,11 @@ import com.palette.palettepetsback.carrot.domain.CarrotLike;
 import com.palette.palettepetsback.member.entity.Member;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
 import java.util.Optional;
 
 public interface CarrotLikeRepository extends JpaRepository<CarrotLike, Long> {
     Optional<CarrotLike> findByCarrotIdAndMember (Carrot carrot, Member member);
+    List<CarrotLike> findByMember (Member member);
+
 }
